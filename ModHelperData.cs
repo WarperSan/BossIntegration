a12d2e22ed118c0b88186c33a1a88577a04ae7d2
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2024-03-03 23:57:10 -0500

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "37.0";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "41.2";
+    public const string Version = "1.0.1";
     public const string Name = "Boss Integration";
 
     public const string Description = "Mod allowing modded bosses to be added more easily.";
